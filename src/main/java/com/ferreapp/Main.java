9776130bd234e60ad41f7ccefa225bb289fd2594
Jaiderpe <perezjaider473@gmail.com>
2025-03-14 16:42:53 -0500

    inteto elaboracion del primer ejercicio

--- src/main/java/com/ferreapp/Main.java
+++ src/main/java/com/ferreapp/Main.java
@@ -1,11 +1,19 @@
 package com.ferreapp;
 
-import com.ferreapp.infrastructure.adapter.ui.EpsUI;
+import java.util.Map;
 
+import com.ferreapp.domain.entities.Eps;
+
+
+import com.ferreapp.infrastructure.adapter.ui.EpsUI;
 
 public class Main {
     public static void main(String[] args) {
         EpsUI epsUI = new EpsUI();
-        epsUI.FindAllEps();
+        Map<Integer, Eps> myEps =epsUI.FindAllEps();
+        // myEps.forEach(((k,v)) -> System.out.println("Id :" + k + "nombre: "+ v.getName()));
+        for (Eps eps : epsUI.FindAllEpsList("Coomeva", myEps)){
+            System.out.println("Id: " + eps.getId() + "Nombre" + eps.getName());
+        }
     }
-}
+}         
